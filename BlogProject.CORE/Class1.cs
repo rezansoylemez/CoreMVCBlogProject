4ed0e7ccab0398fe0117b362669baeb4000416e1
rezansoylemez <rezan.sm@gmail.com>
2022-06-24 16:11:52 +0300

    Core layer map service and entities added

--- BlogProject.CORE/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BlogProject.CORE
-{
-    public class Class1
-    {
-    }
-}
---
+++ BlogProject.CORE/Map/CoreMap.cs
@@ -0,0 +1,27 @@
+﻿using BlogProject.CORE.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.CORE.Map
+{
+    public abstract class CoreMap<T> : IEntityTypeConfiguration<T> where T : CoreEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(z => z.ID);
+            builder.Property(z => z.Status).IsRequired(true);
+            builder.Property(z => z.CreatedComputerName).IsRequired(false).HasMaxLength(255);
+            builder.Property(z => z.CreatedDate).IsRequired(false);
+            builder.Property(z => z.CreatedIP).IsRequired(false).HasMaxLength(15);
+
+            builder.Property(z => z.UpdatedComputerName).IsRequired(false).HasMaxLength(255);
+            builder.Property(z => z.UpdatedDate).IsRequired(false);
+            builder.Property(z => z.UpdatedIP).IsRequired(false).HasMaxLength(15);
+        }
+    }
+}
